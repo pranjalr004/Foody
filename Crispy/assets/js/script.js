9fa375e07c84aaf54726498a94619cee05e12c96
pranjalr004 <pranjalr376@gmail.com>
2025-02-11 08:12:47 +0530

    Navbar section created

---
+++ Crispy/assets/js/script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+// add event listener on multiple events
+
+const addEventOnElements=function(elements,eventType,callback){
+    for(let i=0,len=elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType,callback)
+    }
+}
+
+// For Mobile Nav
+
+const navbar=document.querySelector("[data-navbar]")
+const navbarToggler=document.querySelectorAll("[data-nav-toggler]")
+const navbarLinks=document.querySelectorAll("[data-nav-link]")
+const overlay=document.querySelector("[data-overlay]")
+
+const togglerNav=function(){
+    navbar.classList.toggle("active")
+    overlay.classList.toggle("active")
+}
+
+
+addEventOnElements(navbarToggler,"click",togglerNav)
+
+const closeNav=function(){
+    navbar.classList.remove("active")
+    overlay.classList.remove("active")
+}
+
+addEventOnElements(navbarLinks,"click",closeNav)
+
+// Header
+
+const header=document.querySelector("[data-header]")
+
+window.addEventListener("scroll",function(){
+    header.classList[window.scrollY > 50 ? "add" : "remove"]("active")
+})
